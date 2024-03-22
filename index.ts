0c14ad24b2fd7c615af0f55d79c9a0682592af61
Alizarajpoot <160638895+Alizarajpoot@users.noreply.github.com>
2024-03-23 03:38:20 +0500

    push my lucky_number_gues to github

---
+++ index.ts
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+// compute will generate a random number
+// user input for gussing number
+// compare user input with computer generated number and show result
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+   { name: "userguessednumber",
+     type:"number",
+     message:"please guess a number 1-6:",
+},
+])
+if(answers.userguessednumber === randomNumber){
+console.log("congrulation! you guessed number");
+} else{
+    console.log("you guessed wrong number");
+}
